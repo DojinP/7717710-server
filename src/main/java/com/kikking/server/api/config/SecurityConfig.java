47ac1f124bd824aac508f84f41e93fea45c05af8
DojinP <coah2013@naver.com>
2024-02-13 19:22:08 +0900

    [feat] SecurityConfig 작성
    - 기본적인 요소만 추가

---
+++ src/main/java/com/kikking/server/api/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.kikking.server.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequest ->
+                        authorizeRequest
+//                                .requestMatchers("/**")
+//                                .authenticated()
+                                .requestMatchers("/auth/**")
+                                .permitAll()
+                                .requestMatchers("/user/signup")
+                                .permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
